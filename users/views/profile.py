0186ea035d485cedd27633c6b5c634c3470ca9ff
ShahabAthar25 <shahabathar25@gmail.com>
2026-09-21 15:53:34 +0500

    Added caching to user profile

--- users/views/profile.py
+++ users/views/profile.py
@@ -1,5 +1,7 @@
-from rest_framework import generics
+from django.core.cache import cache
+from rest_framework import generics, status
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.views import Response
 
 from users.permissions import IsOwnerOrReadOnlyPermission
 from users.serializers import UserSerializer
@@ -12,3 +14,28 @@ class RetrieveUpdateUserView(generics.RetrieveUpdateAPIView):
 
     def get_object(self):
         return self.request.user
+
+    def get_cache_key(self):
+        return f"user:{self.request.user.id}:profile"
+
+    def retrieve(self, request, *args, **kwargs):
+        cache_key = self.get_cache_key()
+
+        data = cache.get(cache_key)
+        # Cache miss
+        if not data:
+            instance = self.get_object()
+            serializer = self.get_serializer(instance)
+            data = serializer.data
+            cache.set(cache_key, data, timeout=60 * 60 * 12)
+
+        return Response(data, status=status.HTTP_200_OK)
+
+    def update(self, request, *args, **kwargs):
+        response = super().update(request, *args, **kwargs)
+
+        if response.status_code == status.HTTP_200_OK:
+            # Cache invalidated after update
+            cache.delete(self.get_cache_key())
+
+        return response
